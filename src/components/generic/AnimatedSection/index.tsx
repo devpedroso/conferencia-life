e2e4b083828896c58ca547f0ccb82da6f758f081
devpedroso <filipe172010@hotmail.com>
2024-07-03 01:15:02 -0300

    animacao scroll

--- src/components/generic/AnimatedSection/index.tsx
+++ src/components/generic/AnimatedSection/index.tsx
@@ -3,7 +3,7 @@ import { useInView } from "react-intersection-observer";
 const AnimatedSection = ({ animate = "", children }) => {
   const { ref, inView } = useInView({
     triggerOnce: true, // A animação ocorre apenas uma vez
-    threshold: 0.3, // A porcentagem do elemento que precisa estar visível para ativar a animação
+    threshold: 0.1, // A porcentagem do elemento que precisa estar visível para ativar a animação
   });
 
   return (
